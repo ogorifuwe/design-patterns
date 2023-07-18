8919b6d2c77c28d1e2cca7fb2207016652b392fa
oi <oifuwe@luc.edu>
2023-07-18 14:22:30 -0500

    add the light on command

--- 6-command-pattern/remote/Light.java
+++ 6-command-pattern/remote/Light.java
@@ -2,6 +2,11 @@ package remote;
 
 public class Light {
 
-  public void on() {}
-  public void off() {}
+  public void on() {
+    System.out.println("Light is on");
+  }
+
+  public void off() {
+    System.out.println("Light is off");
+  }
 }
---
+++ 6-command-pattern/remote/RemoteControlTest.java
@@ -0,0 +1,13 @@
+package remote;
+
+public class RemoteControlTest {
+
+  public static void main(String[] args) {
+    SimpleRemoteControl remote = new SimpleRemoteControl();
+    Light light = new Light();
+    LightOnCommand lightOn = new LightOnCommand(light);
+
+    remote.setCommand(lightOn);
+    remote.buttonPressed();
+  }
+} 
