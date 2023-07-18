5831dd70885918fabbb7ed80b98311a222b4ca6e
oi <oifuwe@luc.edu>
2023-07-18 15:51:44 -0500

    add garage door open command

---
+++ 6-command-pattern/remote/GarageDoor.java
@@ -0,0 +1,24 @@
+package remote;
+
+public class GarageDoor {
+
+  public void up() {
+    System.out.println("The garage door is open");
+  }
+
+  public void down() {
+    System.out.println("The garage door is closed");
+  }
+  
+  public void stop() {
+    System.out.println("The garage door is stopped");
+  }
+
+  public void lightOn() {
+    System.out.println("The garage light is on");
+  }
+
+  public void lightOff() {
+    System.out.println("The garage light is off");
+  }
+}
